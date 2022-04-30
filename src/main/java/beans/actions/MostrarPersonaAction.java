bc9f4b2a0fd41dbb04789c2370dff8775c6b0a1b
Daniel Delgado Rodríguez <ddelga4@bancodebogota.com.co>
2022-04-29 22:43:31 -0500

    Upload project

---
+++ src/main/java/beans/actions/MostrarPersonaAction.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.logging.log4j.*;
+
+/**
+ *
+ * @author adseglocdom
+ */
+public class MostrarPersonaAction extends ActionSupport {
+
+    Logger log = LogManager.getLogger(MostrarPersonaAction.class);
+
+    private String nombre;
+
+    @Override
+    public String execute() {
+        log.info("El nombre es:" + nombre);
+        return SUCCESS;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTitulo() {
+        return getText("persona.titulo");
+    }
+
+    public String getValor() {
+        return getText("persona.valor");
+    }
+
+    public String getBoton() {
+        return getText("persona.boton");
+    }
+
+}
